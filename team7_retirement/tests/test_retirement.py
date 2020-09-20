ee309dcfc707a7779c73772bdbd70c63307277ad
mjhorn <mjhorn.personal@gmail.com>
2020-09-19 22:10:17 -0400

    15 more tests!
    Finished testing for each boundary of normal_retirement with valid inputs.

--- team7_retirement/tests/test_retirement.py
+++ team7_retirement/tests/test_retirement.py
@@ -16,7 +16,22 @@ each boundary condition. So for exampmle, next we'd test for 1938, 1939 and so, 
 '''
 @pytest.mark.parametrize("year,month,expected",
                          [(1900,1,("65 and 0 months", 1965, 1)),
-                          (1937,12,("65 and 0 months", 2002, 12))])
+                          (1937,2,("65 and 0 months", 2002, 2)),
+                          (1938,3,("65 and 2 months", 2003, 5)),
+                          (1939,4,("65 and 4 months", 2004, 8)),
+                          (1940,5,("65 and 6 months", 2005, 11)),
+                          (1941,6,("65 and 8 months", 2007, 2)),
+                          (1942,7,("65 and 10 months", 2008, 5)),
+                          (1943,8,("66 and 0 months", 2009, 8)),
+                          (1954,9,("66 and 0 months", 2020, 9)),
+                          (1955,10,("66 and 2 months", 2021, 12)),
+                          (1956,11,("66 and 4 months", 2023, 3)),
+                          (1957,12,("66 and 6 months", 2024, 6)),
+                          (1958,1,("66 and 8 months", 2024, 9)),
+                          (1959,2,("66 and 10 months", 2025, 12)),
+                          (1960,3,("67 and 0 months", 2027, 3)),
+                          (2999,4,("67 and 0 months", 3066, 4))])
+
 def test_normal_retirement_(year, month, expected):
     assert (FullRetirementAge.normal_retirement(year, month) ==
             expected)
